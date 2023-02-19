6921faf42fd7bd49ec878f90de7557e53b4bfe59
darcy <info@darcy-it.com>
2023-02-19 12:10:53 +0900

    ms

--- git-push.sh
+++ git-push.sh
@@ -1,6 +1,5 @@
 #
 
 USER='darcy@'
-#git push https://github.com/darcy-it/git-cmd.git
-git push https://${USER}github.com/darcy-it/git-cmd.git
+git push https://${USER}github.com/darcy-it/ubuntu20.04-beep-sound-onoff.git
 
